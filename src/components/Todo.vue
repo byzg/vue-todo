fea01f01710c90de12e4434b00bc7b564c2d6ea1
Artem <byzgaev@artem-byzgaev.kodep.ru>
2018-02-07 18:09:33 +0300

    load items

---
+++ src/components/Todo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Todo list</h1>
+    <ul>
+      <li v-for="item in items" v-bind:key="item.id">{{item.title}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import request from 'superagent';
+
+  export default {
+    data () {
+      this.items = [];
+
+      return { items: this.items }
+    },
+    created: function() {
+      request
+        .get('https://my-json-server.typicode.com/byzg/rep_react_todo/items')
+        .then(res => this.items = res.body );
+    }
+  }
+</script>

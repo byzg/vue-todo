a612f6a57ee20b8d3abe60b4d4fc43aea438a18f
Artem <byzgaev@artem-byzgaev.kodep.ru>
2018-02-08 13:41:16 +0300

    async await

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -2,31 +2,41 @@
   <div class="todo">
     <h1>Todo list</h1>
     <ul>
-      <li v-for="item in items" v-bind:key="item.id"><todo-item :item="item" @remove="onRemoveItem" /></li>
+      <li v-for="item in items" :key="item.id"><todo-item :item="item" @remove="onRemoveItem" /></li>
     </ul>
+    <todo-form @add="onAddItem"></todo-form>
   </div>
 </template>
 
 <script>
   import request from 'superagent';
   import TodoItem from './TodoItem';
+  import TodoForm from './TodoForm';
 
   export default {
-    components: { TodoItem },
+    components: { TodoItem, TodoForm },
     data () {
       this.items = [];
 
       return { items: this.items }
     },
-    created: function() {
-      request
-        .get('https://my-json-server.typicode.com/byzg/rep_react_todo/items')
-        .then(res => this.items = res.body );
+    // created: async function() {
+    //   request
+    //     .get('http://my-json-server.typicode.com/byzg/rep_react_todo/items')
+    //     .then(res => this.items = res.body );
+    //   // this.items = await request.get('https://my-json-server.typicode.com/byzg/rep_react_todo/items')
+    // },
+    created: async function() {
+      this.items = (await request.get('http://my-json-server.typicode.com/byzg/rep_react_todo/items')).body
     },
     methods: {
       onRemoveItem (item){
         this.items.splice(this.items.indexOf(item), 1);
       //  SOME REQUEST TO SERVER HERE
+      },
+      onAddItem (params) {
+        const item = Object.assign({ id: this.items[this.items.length - 1] + 1 }, params);
+        this.items.push(item)
       }
     }
   }
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <input v-model="title" placeholder="New item title"/>
+    <button @click="add()">Save</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'todo-form',
+    data() {
+      return { title: '' }
+    },
+    methods: {
+      add() {
+        if (this.title) {
+          this.$emit('add', { title: this.title });
+          this.title = ''
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+</style>

6240cd98154304cc84ad487f120ae174f9e7568c
Artem <byzgaev@artem-byzgaev.kodep.ru>
2018-02-09 11:24:14 +0300

    empty TodoItem with styles

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="todo-item">{{item.title}}</div>
+</template>
+
+<script>
+  export default {
+    name: 'TodoItem',
+  }
+</script>
+
+<style lang="scss" scoped>
+  .todo-item {
+    $scope: &;
+    display: flex;
+    &__remove {
+      margin-left: auto
+    }
+    &_checked {
+      #{$scope}__title {
+        text-decoration: line-through;
+      }
+    }
+  }
+</style>
